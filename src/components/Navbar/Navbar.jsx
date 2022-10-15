31a8f710b2fe223bcdd5627f323abb88a4c96f34
Steven Zuluaga <steven000069@hotmail.com>
2022-10-14 19:45:45 -0500

    Agregando estilos al menú de navegación

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Logo from '../../Assets/Logo.png'
+import './Navbar.css'
 
 const Navbar = () => {
      return (
-          <nav>
+          <nav className='navbar'>
+               <div >
+                    <img className='logo-navbar' src={ Logo} alt='Logo de la escuela de código C13'/>
+               </div>
                <div>
-                    <img src=''/>
+                    <ul className='items-navbar'>
+                         <li className='item-navbar'><a  className='link-item' href='#'>Sobre nosotros</a></li>
+                         <li className='item-navbar'><a  className='link-item' href='#'>Carreras</a></li>
+                         <li className='item-navbar'><a className='link-item'  href='#'>Noticias</a></li>
+                         
+                    </ul>
                </div>
           </nav>
      )
