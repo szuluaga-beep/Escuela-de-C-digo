80fb94eab88ccb205ff278e046f7914246c5a382
Steven Zuluaga <steven000069@hotmail.com>
2022-10-14 19:24:36 -0500

    Agregando el menú de navegación

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+     return (
+          <nav>
+               <div>
+                    <img src=''/>
+               </div>
+          </nav>
+     )
+}
+
+export default Navbar
